8526b0dbbbf5afdd7fe4f69714dab9df9a776bda
Yifan WANG <ytwangyifan4@gmail.com>
2018-05-16 00:52:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 function getInput(){
    var longtitude=document.getElementById("longti").value;
    var latitude=document.getElementById("lati").value;
+   console.ingo('longtitude :'+longtitude);
+   console.ingo('latitude :'+latitude);
   }
