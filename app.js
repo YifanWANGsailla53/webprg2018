88c1550faaf3437d1b5d4dd34c1d84215c9d9b9a
Yifan WANG <ytwangyifan4@gmail.com>
2018-05-16 07:32:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js', {scope: '/'})
+            .then(function (registration) {
 
+                // if reg success
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            })
+            .catch(function (err) {
+
+                // if reg fail
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
 
 function getInput(){
    var longtitude=document.getElementById("longti").value;
