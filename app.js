374476b8a9544c1eb651337acc035b7e88f5bdb6
Yifan WANG <ytwangyifan4@gmail.com>
2018-05-16 20:36:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,34 @@ function getInput(){
    console.info('longtitude :'+longtitude);
    console.info('latitude :'+latitude);
   }
+
+function getLocation(){ 
+  if (navigator.geolocation){ 
+    navigator.geolocation.getCurrentPosition(showPosition,showError); 
+  }else{ 
+    alert("navigator do not support geo-targeting"); 
+  } 
+} 
+
+function showError(error){ 
+  switch(error.code) { 
+    case error.PERMISSION_DENIED: 
+      alert("Targeting failed, user refused to request geo-targeting"); 
+      break; 
+    case error.POSITION_UNAVAILABLE: 
+      alert("Targeting failed, geo-targeting info can not be used"); 
+      break; 
+    case error.TIMEOUT: 
+      alert("Targeting failed, time out"); 
+      break; 
+    case error.UNKNOWN_ERROR: 
+      alert("Targeting failed, positioning system failed"); 
+      break; 
+  } 
+}
+
+function showPosition(position){ 
+  var lat = position.coords.latitude; //纬度 
+  var lag = position.coords.longitude; //经度 
+  alert('latitude:'+lat+',longitude:'+lag); 
+} 
