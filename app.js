5bd55ebdb9cade3d1a0c47724ef7bdc56e6e75a9
Yifan WANG <ytwangyifan4@gmail.com>
2018-05-16 08:14:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/sw.js', {scope: '/'})
+        navigator.serviceWorker.register('/webprg2018/sw.js', {scope: '/'})
             .then(function (registration) {
                 // if reg success
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
